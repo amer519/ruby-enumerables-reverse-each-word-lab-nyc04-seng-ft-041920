79e3eab6f0bba9c8e8c9d78daf7c599effc2fceb
Amer Fahmy <afahmy519@gmail.com>
2020-05-02 21:13:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,12 @@ def reverse_each_word(string)
 end
 
 
+def reverse_each_word(string)
+  
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
